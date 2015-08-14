e7c2cea43eceb240aa3f33e7bc2b53283248acdc
RyoYmst <k809231@kansai-u.ac.jp>
2015-08-14 22:21:29 +0900

    トピック内の単語と作品の特徴語の共起語の取得

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function CreateNodeTopic(genre){
-	var $node = $("<div>").text(genre).addClass("topics");
+	var $node = $("<div>").text(genre).addClass("topic");
 	return $node;
 }
 
@@ -13,7 +13,7 @@ function NodesTopic(genres){
 }
 
 function RecommendComicGenres(title,comic_data){
-	for (var i=0;i<comic_data.length;i++){
+	for (var i=0;i < comic_data.length;i++){
 		db_title = " " + comic_data[i].title + " "
 		if(db_title.indexOf(" " + title + " ") !== -1){
 			return comic_data[i]
@@ -21,19 +21,53 @@ function RecommendComicGenres(title,comic_data){
 	}
 }
 
-function spread(topics,r){
-	for (var i=0;i<topics.length; i++){
-		 console.log(topics[i])
-	    // var node = topics[i];
-	    // var centerX = node.offset().left;
-	    // var centerY = node.offset().top;
-	    // var rad = 2*Math.PI * (i/topics.length);
-	    // var x = r * Math.cos(rad) + centerX;
-	    // var y = r * Math.sin(rad) + centerY;
-	    // topics[i].animate({
-	    //   left:x,
-	    //   top:y },"slow");
-	    }
+function RelatedComics(topic,comic_data){
+
+	var related_comics = [];
+	var topic_contents_list = topic.text().split(',');
+	for(var i=0; i <comic_data.length; i++){
+		var related_comic = {};
+		var count = 0;
+		var common_words = [];//各作品の特徴とトピックの語との共起語のlist
+
+		for(j=0; j <topic_contents_list.length; j++){
+			if ($.inArray(topic_contents_list[j],comic_data[i].genres) !== -1){//topic内の各単語と対象作品の特徴語とを比較
+				if (count > 1){
+					related_comic["title"] = comic_data[i].title;
+					related_comic["genres"] = comic_data[i].genres;	
+				}else{
+					count = count + 1 ;
+				}
+				common_words.push(topic_contents_list[j])
+			}
+		}
+
+		if (Object.keys(common_words).length > 1){
+			related_comic["common_words"] = common_words;
+		}
+		
+		if (Object.keys(related_comic).length !== 0){
+			related_comics.push(related_comic)
+		}
+	}
+	console.log(related_comics)
+}
+
+
+function Spread(topics,r,comic_data){
+	for (var i = 0; i < topics.length; i++){
+	    var node = topics[i];
+	    var centerX = node.offset().left;
+	    var centerY = node.offset().top;
+	    var rad = 2*Math.PI * (i/topics.length);
+	    var x = r * Math.cos(rad) + centerX + 64/4;
+	    var y = r * Math.sin(rad) + centerY + 91/4;
+	    test = RelatedComics(topics[i],comic_data)
+	    topics[i].animate({
+	      left:x,
+	      top:y
+	    },"slow");
+	}
 }
 
 $(function(){
@@ -43,17 +77,10 @@ $(function(){
 	$(document).on("click","#input_button",function(){
 	  	var recommend_comic = ExpectLikeComic();
 	  	var recommend_comic_gernes = RecommendComicGenres(recommend_comic[0],comic_data).genres;
-	  	var $genre = $("#genres");
-	  	nodes_genre = NodesTopic(recommend_comic_gernes);
-	  	for (var i = 0; i< nodes_genre.length; i++){
-	  		var each_genre = nodes_genre[i];
-	  		$genre.append(each_genre);
-	  	}
 
-	  	recommend_like_topics = []
-	  	for (var i =0 ; i< topic_data.length; i++){
-	  		var count = 0;
-	  		for (var j = 0; j< recommend_comic_gernes.length; j++){
+	  	recommend_like_topics = []//共起する語が含まれるtopic
+	  	for (var i = 0 ; i < topic_data.length; i++){
+	  		for (var j = 0; j < recommend_comic_gernes.length; j++){
 	  			if ($.inArray(recommend_comic_gernes[j],topic_data[i].genre) !== -1){
 	  				recommend_like_topics.push(topic_data[i].genre)
 	  				break;
@@ -61,24 +88,28 @@ $(function(){
 	  		}
 	  	}
 
+		nodes_topics = NodesTopic(recommend_like_topics);//関連topicのdiv
+		
 	  	var $topics = $("#topics");
 	  	var $expect_like_comic = $(".center");//中央に提示されるコミック
         var pos = $expect_like_comic.position();
-        console.log(pos);
-        var centerX = pos.left;
-        var centerY = pos.top;
-	  	for (var i = 0; i< recommend_like_topics.length; i++){
-	  		var each_topics = "hoge";//recommend_like_topics[i];
-	  		// $topics.append(each_topics);
-	  	
-	  	}
-	  	r = 200;
-	  	spread(recommend_like_topics,r)
-	  	
+        var centerX = pos.left;//中央のコミックのX座標
+        var centerY = pos.top;//中央のコミックのY座標
 
+	  	for (var i = 0; i < nodes_topics.length; i++){
+	  		var $each_topics = nodes_topics[i];
+	  		$topics.append($each_topics);	
+	  		$each_topics.css({
+	  			left:centerX,
+    			top:centerY,
+	  		})
+		}
 
+	  	r = 100;
+	  	// console.log(comic_data)
+	  	Spread(nodes_topics,r,comic_data)
 	  	$("#like_comic_titles").remove();
-
-
 	});
 })
+
+
