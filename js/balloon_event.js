f512b1d1b8380d0d63b216f08fef958152669ad9
RyoYmst <k809231@kansai-u.ac.jp>
2015-08-21 17:38:00 +0900

    バルーン修正

--- js/balloon_event.js
+++ js/balloon_event.js
@@ -1,8 +1,24 @@
+function SplitWords(topic_word_list){
+	split_topic_word_list = [];
+	for(var i = 0; i < topic_word_list.length; i++){
+		split_topic_word_list.push(" " + topic_word_list[i] + " ")
+	}
+	return split_topic_word_list
+}
+
+////////////////////////////////////////////
+//各トピックのマウスイベント処理(バルーン)
+////////////////////////////////////////////
 
 $(document).on("mouseover",".topic",function(){
-
+	var split_topic_word = SplitWords($(this).text().split(","))
 	$(this).showBalloon({
-		contents:"<div id = balloon>" + $(this).text() + "</div>"
+		contents:"<div id = balloon_tipic>" + split_topic_word_list + "</div>",
+		css:{
+			fontSize:"24px",
+			width:"500px",
+			backgroundColor:"#CCFFFF"
+		}
 	});
 })
 
@@ -10,21 +26,49 @@ $(document).on("mouseout",".topic",function(){
 	$(this).hideBalloon();
 })
 
+////////////////////////////////////////////
+//関連コミックのマウスイベント処理(バルーン)
+////////////////////////////////////////////
 
 $(document).on("mouseover",".related_comic",function(){
+	match_data = [];
+	for (var i = 0; i < comic_data.length; i++){
+		if ($(this).text().indexOf(comic_data[i].title) !== -1){
+			match_data.push(comic_data[i])
+			break
+		}
+	} 
+	var split_topic_word = SplitWords(match_data[0].genres)
+
 	$(this).showBalloon({
-		contents:"<div id = balloon>"+ $(this).text() +"</div>"
+		contents:"<div id = balloon_related><div id = balloon_title>" + match_data[0].title + "</div><br>" + split_topic_word + "</div>",
+		css:{
+			width:"500px",
+			backgroundColor:"#FFCCFF"
+		}
 	});
+	$(this).animate({
+		width:"153.6px",
+		height:"218.4px"
+	},"500");
+
+
+
+
+
 })
 
 $(document).on("mouseout",".related_comic",function(){
 	$(this).hideBalloon();
+	$(this).animate({
+		width:"38.4px",
+		height:"54.6px"
+	},"500");
 })
 
-$(document).on("click",".topic",function(){
-	console.log($(this).text())
-})
-
+////////////////////////////////////////////
+//中央提示コミックのイベント処理(バルーン)
+////////////////////////////////////////////
 
 $(document).on("mouseover",".center",function(){
 	match_data = [];
@@ -34,10 +78,13 @@ $(document).on("mouseover",".center",function(){
 			break
 		}
 	}
+	var split_topic_word = SplitWords(match_data[0].genres)
 	$(this).showBalloon({
-		contents:"<div id = balloon>" + "<div id = balloon_title>" + match_data[0].title+ "</div>" + "<br>" + match_data[0].genres + "</div>",
+		contents:"<div id = balloon_center><div id = balloon_title>" + match_data[0].title+ "</div><br>" + split_topic_word + "</div>",
 		css:{
+			width:"500px",
 			backgroundColor:"#F8DC85",
+			fontColor:"black"
 		}
 	});
 })
